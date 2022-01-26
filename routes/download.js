6f20a6e55feb880b2e15199b1d5e6d35637ba0cb
Aditya Sonani <adityasonani19@gmail.com>
2022-01-26 19:19:07 +0530

    Minor change in download.js

--- routes/download.js
+++ routes/download.js
@@ -3,14 +3,17 @@ const File = require('../models/file');
 
 router.get('/:uuid', async (req, res)=>{
     // console.log("here in /files/download/uuid");
-    const file = await File.findOne({ uuid: req.params.uuid });
-    if (!file) {
-        return res.render('download', {error: 'Link has been expired or please check the url.'});
+    try {
+        const file = await File.findOne({ uuid: req.params.uuid });
+        if (!file) {
+            return res.render('download', {error: 'Link has been expired or please check the url.'});
+        }
+
+        const filePath = `${__dirname}/../${file.path}`;
+        res.download(filePath);
+    } catch(err) {
+        return res.status(500).send({ error: 'Something went wrong.'});
     }
-
-    const filePath = `${__dirname}/../${file.path}`;
-    res.download(filePath);
-
 });
 
 module.exports = router;
