f6ad76e3dad81e6f6c9f96345392285a2bc44e53
adityasonani <70087338+adityasonani@users.noreply.github.com>
2022-01-29 10:38:42 +0530

    cors update

--- server.js
+++ server.js
@@ -11,23 +11,10 @@ app.use(express.static('public'));
 const connectDB = require('./config/db');
 connectDB();
 
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-};
-
-app.use(cors(corsOptions));
-
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // handle OPTIONS method
-    if ('OPTIONS' == req.method) {
-        return res.sendStatus(200);
-    } else {
-        next();
-    }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
